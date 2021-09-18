993132ee4ec430f87f4ff7de41f9c79602355914
Sona Mathew <sonamathewc@gmail.com>
2021-09-18 13:16:34 +0530

    initial commit

---
+++ assets/js/affordable.js
@@ -0,0 +1,6 @@
+ScrollLottie({
+    target: "#affordabe-img",
+    path: 'https://assets9.lottiefiles.com/packages/lf20_996gclxd.json',
+    duration: 2,
+    speed: 'medium'
+})
---
+++ assets/js/goodhealth.js
@@ -0,0 +1,6 @@
+ScrollLottie({
+    target: "#goodhealth",
+    path: 'https://assets4.lottiefiles.com/packages/lf20_m8sr1qgw.json',
+    duration: 2,
+    speed: 'medium'
+})
